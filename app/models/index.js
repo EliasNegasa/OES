7a090edebeaea04b1b34586806485f00912f9d47
EliasNegasa <eliasnegasa5@gmail.com>
2023-04-10 16:46:30 +0300

    first commit

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+import config from "../config/db";
+import Sequelize from "sequelize";
+
+const sequelize = new Sequelize(config.DB, config.HOST, config.PASSWORD, {
+  host: config.HOST,
+  dialect: config.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: config.pool.max,
+    min: config.pool.min,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
+  },
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import router from "./app/routes";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
+});
