732cfb87b33bace3592302b68257b95baa92d12f
EliasNegasa <eliasnegasa5@gmail.com>
2023-04-10 16:52:55 +0300

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use("/auth", authRouter);
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
